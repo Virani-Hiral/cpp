2911b9613ff1c63062684f3c0ecf1c4827428b8b
Virani-Hiral <142477714+Virani-Hiral@users.noreply.github.com>
2023-08-22 19:39:35 +0530

    Create p1-4.cpp

---
+++ p1-4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+  
+
+int main()
+{
+	cout<<"Virani Hiral"<<endl;
+	cout<<"220130318062"<<endl;
+
+    string name;
+      
+    cout << "Enter the name: ";
+    cin >> name;
+    
+    cout << "Entered name is: " << 
+           name; 
+    return 0;
+}
+
+
