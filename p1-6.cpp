bef3b7d733c367e32cd9f02605ee301397db0ce7
Virani-Hiral <142477714+Virani-Hiral@users.noreply.github.com>
2023-08-26 08:22:10 +0530

    Create p1-6.cpp

---
+++ p1-6.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{ 
+	cout<<"Virani Hiral"<<endl;
+	cout<<"220130318062"<<endl;   
+	
+	cout << "Size of char: " << sizeof(char) << " byte" << endl;
+    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+
+    return 0;
+}
+
