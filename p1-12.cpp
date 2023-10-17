7c385971e582421bb50db98a1fa5115b2c16c74c
Virani-Hiral <142477714+Virani-Hiral@users.noreply.github.com>
2023-10-17 08:29:48 +0530

    Create p1-12.cpp

---
+++ p1-12.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Virani Hiral"<<endl;
+	cout<<"220130318062"<<endl;
+
+	int n = 6;
+	for (int i = 1; i <= 10; ++i)
+		cout << n << " * " << i << " = " << n * i << endl;
+	return 0;
+}
