0bdaffa4aa57d5f470bdca3819bb15410027ee84
Virani-Hiral <142477714+Virani-Hiral@users.noreply.github.com>
2023-10-17 08:20:38 +0530

    Create p1-9.cpp

---
+++ p1-9.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Virani Hiral"<<endl;
+	cout<<"220130318062"<<endl;
+
+  	int n;
+
+  	cout << "Enter an integer: ";
+  	cin >> n;
+
+  	if ( n % 2 == 0)
+    cout << n << " is even.";
+  	else
+    cout << n << " is odd.";
+
+  return 0;
+}
